a34206f88d00613c416eaea6d6668d4611113057
mr.Escow <escowxbox@gmail.com>
2022-04-29 00:43:29 +0300

    Update for test massenges

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 // IN PRO FILE: LIBS += D:\Qt\QT6\Tools\mingw1120_64\x86_64-w64-mingw32\lib\libws2_32.a  !!!
 
+
 #include <QApplication>
-
 #include <winsock2.h>
-
 #include <QDebug>
+#include <thread>
 
+
+const auto buffer_size = 1024;
 SOCKET Connection;
 
-void ClientHandler(){
-    char msg[256];
+std::thread Client_Handler([](){
+    int SizeMsg{0};
+    QVector<char> clientBuff;
+    clientBuff.resize(buffer_size);
     while(true){
-        recv(Connection,msg, sizeof(msg),NULL);
-        qDebug() << msg;
+        if(recv(Connection, clientBuff.data(), clientBuff.size(), NULL) != SOCKET_ERROR){
+            char* msg = new char[SizeMsg + 1];
+            msg[SizeMsg] = '\0';
+            if( recv(Connection, msg, SizeMsg, 1) != SOCKET_ERROR){
+                qDebug() << "MSG:" << clientBuff.data() << "SIZE:" << clientBuff.size();
+            }
+            delete[] msg;
+        }
     }
-}
+});
+
+
 
 int main(int argc, char *argv[])
 {
@@ -32,30 +44,55 @@ int main(int argc, char *argv[])
     addr.sin_port = htons(1989);
     addr.sin_family = AF_INET;
 
-    Connection = socket(AF_INET,SOCK_STREAM,NULL);
+    Connection = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+
     // connect != Qt connect because function is reset! :)
-    if(connect(Connection,(SOCKADDR*)&addr,sizeof(addr)) != 0){
+//    if(connect(Connection,(SOCKADDR*)&addr,sizeof(addr)) != 0){
+//        qDebug() << "Error: failed connect to server";
+//        return a.exec();
+//    }
+//    else{
+//        qDebug() << "Connected!";
+//        qDebug() << "Client discriptor " << Connection;
+//        Sleep(100);
+
+//        //CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)ClientHandler,NULL,NULL,NULL);
+//        Client_Handler.detach();
+//    }
+
+    while(connect(Connection,(SOCKADDR*)&addr,sizeof(addr)) != 0){
         qDebug() << "Error: failed connect to server";
-        return a.exec();
+        Sleep(1000);
     }
+
     qDebug() << "Connected!";
+    qDebug() << "Client discriptor " << Connection;
+    Sleep(100);
 
-    char msg[256];
-    recv(Connection, msg, sizeof(msg), NULL);
-    qDebug() << msg;
+    //CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)ClientHandler,NULL,NULL,NULL);
+    Client_Handler.detach();
 
-    CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)ClientHandler,NULL,NULL,NULL);
+    //QTextStream cin(stdin);
+    //QString ClientMsg;
+    //int SizeClientMsg;
 
-    QTextStream cin(stdin);
-    QString ClientMsg;
-    int SizeClientMsg;
+    QVector<char> buffer;
+    QString ClientMsg = "HELLO MY";// SERVER!!!";
+
+    buffer.resize(ClientMsg.size());
+
+    for(int i = 0; i < ClientMsg.size();i++){
+        buffer[i] = ClientMsg.toStdString()[i];
+    }
 
     while(true){
-        ClientMsg = cin.readLine();
-        SizeClientMsg = ClientMsg.size();
-        send(Connection, (char*)SizeClientMsg, sizeof(int),NULL);
-        send(Connection, ClientMsg.toStdString().c_str(), sizeof(ClientMsg), NULL);
-        Sleep(10);
+        //ClientMsg = cin.readLine();
+        //SizeClientMsg = ClientMsg.size();
+        //send(Connection, (char*)SizeClientMsg, sizeof(int),NULL);
+        //send(Connection, ClientMsg.toStdString().c_str(), sizeof(ClientMsg), NULL);
+
+        send(Connection, buffer.data(), buffer.size(), NULL);
+        Sleep(1000);
     }
 
     qDebug() << "exit";
